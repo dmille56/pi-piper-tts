da7b539cb6783bb466d5ad64cb89d1578ccaa045
Donovan M <donovanm56@gmail.com>
2026-05-14 01:47:01 -0700

    removing unneeded pi-extension-settings package

--- extensions/pi-extension-settings-shim.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "pi-extension-settings/sdk" {
-	export * from "@axnic/pi-extension-settings-sdk";
-}
-
-declare module "pi-extension-settings/sdk/hooks" {
-	export * from "@axnic/pi-extension-settings-sdk/hooks";
-}
